93ecc710330d858ccd92f3098d175025beadcbc9
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 18:35:56 +0200

    routing

---
+++ src/app/pricing/pricing/pricing.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing',
+  templateUrl: './pricing.component.html',
+  styleUrls: [
+    './pricing.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class PricingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
